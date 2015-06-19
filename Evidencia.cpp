9a1feeb87a40a2146f8a8365afbf74b2f8c58bb3
Christian Najera <christiannajera@Christians-MacBook-Pro.local>
2015-06-19 15:52:46 -0600

    Agregado de evidencia (Hpp y Cpp)

---
+++ Evidencia.cpp
@@ -0,0 +1,59 @@
+# include "Evidencia.hpp"
+# include <string>
+# include <sstream>
+
+using std::string;
+using namespace std;
+
+Evidencia::Evidencia(string nombre, string tipo, string lugar, bool huellas, bool procesado){
+	this->nombre = nombre;
+	this->tipo = tipo;
+	this->lugar = lugar;
+	this->huellas = huellas;
+	this->procesado = procesado;
+}
+Evidencia::Evidencia(){}//constructor vacio
+Evidencia::~Evidencia(){}//sin uso
+string Evidencia::getNombre()const{
+	return nombre;
+}
+string Evidencia::getTipo()const{
+	return tipo;
+}
+string Evidencia::getLugar()const{
+	return lugar;
+}
+bool Evidencia::isPrinted()const{
+	return huellas;
+}
+bool Evidencia::isProcessed()const{
+	return procesado;
+}
+void Evidencia::setNombre(string nombre){
+	this->nombre = nombre;
+}
+void Evidencia::setTipo(string tipo){
+	this->tipo = tipo;
+}
+void Evidencia::setHuellas(bool huellas){
+	this->huellas = huellas;
+}
+void Evidencia::setProcesado(bool procesado){
+	this->procesado = procesado;
+}
+string Evidencia::toString()const{
+	stringstream ss;
+	ss << "Nombre: " << nombre
+	<< "\nTipo: " << tipo
+	<< "\nLugar: " << lugar;
+	if(huellas == true)
+		ss << "\nTiene huellas";
+	else
+		ss << "\nNo tiene huellas";
+	if(procesado == true)
+		ss << "\nEsta procesado";
+	else
+		ss << "\nNo esta procesado";
+
+	return ss.str();
+}
